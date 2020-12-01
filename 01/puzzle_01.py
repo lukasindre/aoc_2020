a298e90c35ca1947304175fb68da24f454cb5a5b
Lukas Indre <lukas.indre@upstart.com>
2020-12-01 08:56:34 -0500

    add empty last line

--- 01/puzzle_01.py
+++ 01/puzzle_01.py
@@ -9,3 +9,4 @@ for line in fileinput.input():
 for combination in itertools.combinations(numbers, 2):
     if combination[0] + combination[1] == 2020:
         print(combination[0] * combination[1])
+
