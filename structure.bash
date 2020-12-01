290845dcc9180c9e84a51093c1b44162d87fdf3c
Lukas Indre <lukas.indre@upstart.com>
2020-12-01 18:48:42 -0500

    restructure to make unix gods happy

--- structure.bash
+++ structure.bash
@@ -4,6 +4,12 @@ n=1
 MAX=25
 while [ "$n" -le "$MAX" ]
 do
-	mkdir "$n" && touch "$n/input" && touch "$n/puzzle_01.py" && touch "$n/puzzle_02.py"
+	if [[ $n -lt 10 ]]
+	then
+		mkdir "0${n}" && touch "0${n}/input" && touch "0${n}/puzzle_01.py" && touch "0${n}/puzzle_02.py"
+	else
+		mkdir "$n" && touch "$n/input" && touch "$n/puzzle_01.py" && touch "$n/puzzle_02.py"
+	fi
+
 	n=`expr "$n" + 1`
 done
