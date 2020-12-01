e11ae9024e86f1363663ed97c263433debbc0854
Lukas Indre <lukas.indre@upstart.com>
2020-12-01 08:56:11 -0500

    finish puzzle 1

--- 01/puzzle_01.py
+++ 01/puzzle_01.py
@@ -1,7 +1,11 @@
 import fileinput
+import itertools
 
 numbers = []
 
 for line in fileinput.input():
     numbers.append(int(line))
 
+for combination in itertools.combinations(numbers, 2):
+    if combination[0] + combination[1] == 2020:
+        print(combination[0] * combination[1])
