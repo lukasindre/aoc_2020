d48d9bc0b220dbd20109c1784af51de8659feda9
Lukas Indre <lukas.indre@upstart.com>
2020-12-08 08:53:28 -0500

    testing

--- 08/puzzle_02.py
+++ 08/puzzle_02.py
@@ -1,16 +1,14 @@
 import fileinput
-from collections import Counter
 from pprint import pprint as pp
 
 
 def send_it(rules):
     i = 0
     accumulator = 0
-    actions = []
+    stop_loop_count = 0
    
-    while i < len(rules):
-        actions.append(i)
-        if list(dict(Counter(actions)).values()).count(2) >= 1:
+    while stop_loop_count < len(rules):
+        if i == len(rules) - 1:
             print(accumulator)
             break
         elif rules[i][0] == 'acc':
@@ -21,6 +19,20 @@ def send_it(rules):
         else:
             i += 1
 
+        stop_loop_count += 1
+
 data = [line.rstrip().split() for line in fileinput.input()]
 
-send_it(data)
+for counter in range(len(data)):
+    this_try = data[:]
+    
+    if data[counter][0] == 'acc':
+        continue
+    
+    if data[counter][0] == 'jmp':
+        this_try[counter][0] = 'nop'
+    elif data[counter][0] == 'nop':
+        this_try[counter][0] = 'jmp'
+
+    send_it(this_try)
+
